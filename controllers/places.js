8a0d5d038287831b13d0192cefe699c9679b8c35
suttonj07 <suttonj07@gmail.com>
2024-04-24 12:16:18 -0400

    able to add/pic still not showing

--- controllers/places.js
+++ controllers/places.js
@@ -1,33 +1,31 @@
 const router = require('express').Router()
-
+const places = require('../models/place.js')
 // GET /places
+
+router.get('/', (req, res) => {
+    res.render('places/index', {places})
+})
+
 
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
 
   router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+       if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
-  
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-ml-tables.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-cat.jpg'
-      }]
-    res.render('places/index', {places})
 
-  })
   
 module.exports = router
--- models/place.js
+++ models/place.js
@@ -1,15 +1,29 @@
-places = [{
-	name: 'H-Thai-ML',
-	city: 'Seattle',
-	state: 'WA',
-	cuisines: 'Thai, Pan-Asian',
-	pic: '/images/h-thai-ml-tables.jpg'
-    }, {
-	name: 'Coding Cat Cafe',
-	city: 'Phoenix',
-	state: 'AZ',
-	cuisines: 'Coffee, Bakery',
-	pic: '/images/coffee-cat.jpg'
-}]
+// places = [{
+// 	name: 'H-Thai-ML',
+// 	city: 'Seattle',
+// 	state: 'WA',
+// 	cuisines: 'Thai, Pan-Asian',
+// 	pic: '/images/h-thai-ml-tables.jpg'
+//     }, {
+// 	name: 'Coding Cat Cafe',
+// 	city: 'Phoenix',
+// 	state: 'AZ',
+// 	cuisines: 'Coffee, Bakery',
+// 	pic: '/images/coffee-cat.jpg'
+// }]
 
-module.exports = places;
+// module.exports = places;
+
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/h-thai-ml-tables.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg'
+}]
