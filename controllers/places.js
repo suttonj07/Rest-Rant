6366ff9ea71c1592d288a213632b87bc5fb582c4
suttonj07 <suttonj07@gmail.com>
2024-04-24 21:01:44 -0400

    delete function implemented

--- controllers/places.js
+++ controllers/places.js
@@ -23,7 +23,21 @@ router.get('/new', (req, res) => {
       res.render('error404')
     }
     else {
-        res.render('places/show', { place: places[id] })
+      res.render('places/show', { place: places[id], id })
+    }
+  })
+  
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
     }
   })
   
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,15 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show ({place, id}) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ place.name }</h1>
+            <a href={`/places/${id}/edit`} className="btn btn-warning">Edit</a>
+            <form method="POST" action={`/places/${id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">Delete</button>
+            </form>    
           </main>
         </Def>
     )
