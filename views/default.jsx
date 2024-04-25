ff08200d9070a735cb8743f64e1681d4de651d23
suttonj07 <suttonj07@gmail.com>
2024-04-24 20:13:20 -0400

    stylebar

--- views/default.jsx
+++ views/default.jsx
@@ -10,6 +10,20 @@ function Def (html) {
                 <link rel="stylesheet" href="../public/css/style.css"></link>
             </head>
             <body>
+            <nav>
+                <ul>
+                    <li>
+                    <a href="/">Home</a>
+                    </li>
+                    <li>
+                    <a href="/places">Places</a>
+                    </li>
+                    <li>
+                    <a href="/places/new">Add Place</a>
+                    </li>
+                </ul>
+                </nav>
+
                 {html.children}
             </body>
         </html>
